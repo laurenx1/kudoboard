955405b236e64f9c3613f95bc4c10e350049a735
Lauren Nia Pryor <lnpryor@meta.com>
2024-06-18 11:08:39 -0700

    working search bar

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,3 @@ const PORT = 5173
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
   })
-
-  app.get('/', (req, res) => {
-    res.send('Welcome to my app!')
-  })
